cd3b6347dc13015339cd90521885b004bd55b3e3
Евгений <evgkostovar@gmail.com>
2024-05-26 21:35:31 +0300

    Commit1

--- src/Lesson_2.java
+++ src/Lesson_2.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class Main {
+public class Lesson_2 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         printThreeWords();
